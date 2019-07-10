52a8d5b19c8d0aa3a65d5bfb65717a71c72a2fa3
Ravikiran <Ravikiran@192.168.2.13>
2019-07-10 08:29:43 +0530

    UPDATE RUNNING SERVICE

---
+++ Learn-spring-rest-full-api/src/main/java/io/java/spring/leatrn/CourceAPI.java
@@ -0,0 +1,15 @@
+package io.java.spring.leatrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourceAPI {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(CourceAPI.class, args);
+
+	}
+
+}
