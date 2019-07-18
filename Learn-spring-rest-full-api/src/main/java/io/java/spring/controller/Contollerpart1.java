9d245b86397c7f072056faa505410b0e59abad1c
Ravikiran <Ravikiran@192.168.2.13>
2019-07-18 07:28:48 +0530

    Encrypring final

--- Learn-spring-rest-full-api/src/main/java/io/java/spring/controller/Contollerpart1.java
+++ Learn-spring-rest-full-api/src/main/java/io/java/spring/controller/Contollerpart1.java
@@ -1,17 +1,32 @@
 package io.java.spring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@EnableAutoConfiguration
 public class Contollerpart1 {
+	private String encryptedstring;
 	
-	@RequestMapping(value="/hello", method = RequestMethod.GET )
+	@GetMapping("/")
 	@ResponseBody
-	public String satyhello2() {
-		return "Wlencome to spring";
+	public String encryption(@RequestParam(value = "psw") String psw) {
+		
+		encryptedstring = "";
+		 char c;
+		 int size = psw.length()-1;
+		 
+		 for(int i=0;i<=size;i++) {
+			 c= psw.charAt(i);
+			 c= (char) ((int)c-i+2);
+			 encryptedstring += Character.toString(c);
+		 }
+		
+		return encryptedstring;
+		
 	}
 
 }
--- Learn-spring-rest-full-api/src/main/java/io/java/spring/leatrn/CourceAPI.java
+++ Learn-spring-rest-full-api/src/main/java/io/java/spring/leatrn/CourceAPI.java
@@ -2,9 +2,12 @@ package io.java.spring.leatrn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class CourceAPI {
+@SpringBootApplication(scanBasePackages="io.java.spring")
+public class CourceAPI extends SpringBootServletInitializer{
+	
 
 	public static void main(String[] args) {
 		
